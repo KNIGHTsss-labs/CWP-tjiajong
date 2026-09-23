02c31f300811ef42e83ec920121da4f902d942be
H.M. <jame.jame9424@gmail.com>
2026-09-23 18:51:21 +0700

    finished cell 30 - 25

---
+++ cell00/ex00/to25.py
@@ -0,0 +1,7 @@
+num = int(input("Enter a number less than 25\n"))
+if num >= 25:
+    print("Error")
+elif num < 25:
+    while num <= 25:
+        print("Inside the loop, my variable is ", num)
+        num+=1
